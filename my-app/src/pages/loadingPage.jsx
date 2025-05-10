932514a43ba3f90436f22c41d50b9000bdcce34a
Mohan D <mohan141729@gmail.com>
2025-05-10 13:57:05 +0530

    update frontend

--- my-app/src/pages/loadingPage.jsx
+++ my-app/src/pages/loadingPage.jsx
@@ -1,12 +1,12 @@
 // loadingPage.jsx
 import React from "react";
-import Navbar from "../components/navbar";
-import HeroSection from "../components/heroSection";
-import AboutSection from "../components/aboutSection";
-import ProjectSection from "../components/projectSection";
+import Navbar from "../components/Navbar";
+import HeroSection from "../components/HeroSection";
+import AboutSection from "../components/AboutSection";
+import ProjectSection from "../components/ProjectSection";
 import SkillsSection from "../components/SkillsSection";
-import ServicesSection from "../components/servicesSection";
-import ContactSection from "../components/contactSection";
+import ServicesSection from "../components/ServicesSection";
+import ContactSection from "../components/ContactSection";
 import FooterSection from "../components/FooterSection";
 
 const LoadingPage = () => {
