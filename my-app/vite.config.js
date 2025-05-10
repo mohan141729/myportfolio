45b618963a8493daf048819f4a2519210d7a88b8
Mohan D <mohan141729@gmail.com>
2025-05-10 14:47:47 +0530

    Fix: Configure Tailwind CSS properly

--- my-app/vite.config.js
+++ my-app/vite.config.js
@@ -5,6 +5,9 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  css: {
+    postcss: './postcss.config.js',
+  },
   resolve: {
     preserveSymlinks: true,
     caseSensitive: true,
