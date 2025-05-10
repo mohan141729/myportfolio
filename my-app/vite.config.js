23d56df2146a9572b3213ebc85a8cdc44219668f
Mohan D <mohan141729@gmail.com>
2025-05-10 14:14:55 +0530

    update frontend

--- my-app/vite.config.js
+++ my-app/vite.config.js
@@ -2,9 +2,21 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(),
     tailwindcss(),
   ],
+  resolve: {
+    preserveSymlinks: true,
+    caseSensitive: true
+  },
+  build: {
+    sourcemap: true,
+    rollupOptions: {
+      output: {
+        manualChunks: undefined
+      }
+    }
+  }
 })
